800aff21350cfa218aeeadc1d1504f66406e3bbb
minch02 <alscksgur2@naver.com>
2025-11-20 02:38:37 +0900

    프로필 사진 데이터 추가 및 사용자 데이터 동기화 기능 구현

--- data/characters.js
+++ data/characters.js
@@ -1,3 +1,32 @@
+// 프로필 사진 데이터
+export const profileImages = [
+    {
+        id: 1,
+        name: '기본',
+        image: require('../assets/images/avatar1.png')
+    },
+    {
+        id: 2,
+        name: '빛나는',
+        image: require('../assets/images/avatar2.png')
+    },
+    {
+        id: 3,
+        name: '따뜻한',
+        image: require('../assets/images/avatar3.png')
+    },
+    {
+        id: 4,
+        name: '밤하늘',
+        image: require('../assets/images/avatar4.png')
+    },
+    {
+        id: 5,
+        name: '상쾌한',
+        image: require('../assets/images/avatar5.png')
+    }
+];
+
 // 캐릭터 데이터
 export const characters = [
     {
@@ -39,6 +68,11 @@ export const defaultCharacter = {
 // 캐릭터 ID로 캐릭터 찾기
 export const getCharacterById = (id) => {
     return characters.find(char => char.id === parseInt(id));
+};
+
+// 프로필 사진 ID로 찾기
+export const getProfileImageById = (id) => {
+    return profileImages.find(img => img.id === parseInt(id));
 };
 
 // 기본 캐릭터 또는 선택된 캐릭터 반환
